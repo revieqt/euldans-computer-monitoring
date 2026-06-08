d6f10bdf2d225333fa54e9d637bed278f3e14dac
Joshua Opsima <opsima.josh@gmail.com>
2026-06-08 16:05:32 +0800

    initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Computer Shop Client
+      </h1>
+    </div>
+  );
+}
